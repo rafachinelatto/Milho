377cae4134cf0c52d7277caf5bf42e7c9e160f80
Rafael Antonio Chinelatto <rafachinelatto@gmail.com>
2023-10-03 09:30:19 -0300

    Initial Commit

---
+++ Milho/MilhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilhoApp.swift
+//  Milho
+//
+//  Created by Rafael Antonio Chinelatto on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MilhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
