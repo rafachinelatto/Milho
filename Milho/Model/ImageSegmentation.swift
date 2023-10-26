ce83223890aae6b808eb287d4613657782f32d2a
Rafael Antonio Chinelatto <rafachinelatto@gmail.com>
2023-10-26 10:56:58 -0300

    Face segmentation implemented

--- Milho/Model/ImageSegmentation.swift
+++ Milho/Model/ImageSegmentation.swift
@@ -13,24 +13,29 @@ import CoreImage.CIFilterBuiltins
 class ImageSegmentation: ObservableObject {
     
     var inputImage: UIImage = UIImage()
+    
+    var secondPartInputImage: UIImage = UIImage()
     @Published var outputImage: UIImage?
+    @Published var quality: Float?
     
+    var faceContourPath: Path?
     
-    // Função responsável por isolar a pessoa para futura análise.
+    // Função resposnsável pela segmentação da imagem.
     func segmentImage() {
         
         guard let image = inputImage.cgImage else { return }
         
-        // Cria um request da biblioteca Vision.
+        // Criar um request do Vision
         let request = VNGeneratePersonSegmentationRequest()
         request.revision = VNGeneratePersonSegmentationRequestRevision1
         request.qualityLevel = VNGeneratePersonSegmentationRequest.QualityLevel.accurate
         request.outputPixelFormat = kCVPixelFormatType_OneComponent8
         
-        // Crio o request handler, que é utilizado para chamar o nosso request.
+        // Criar o request handler
         let requestHandler = VNImageRequestHandler(cgImage: image, orientation: .up, options: [:])
         
-        // Processa o request.
+        // Processa o request de forma assíncrona
+     
         do {
             try requestHandler.perform([request])
         } catch {
@@ -38,17 +43,18 @@ class ImageSegmentation: ObservableObject {
         }
     
         
-        // Processa os resultados.
+        // Processa os resultados
         guard let result = request.results, let mask = result.first else { return }
         
         removeBackgound(filterMask: mask.pixelBuffer)
     }
     
+    
     private func removeBackgound(filterMask: CVPixelBuffer) {
         
         
         guard let image = inputImage.cgImage else { return }
-        guard let bgImage = UIImage.imageFromColor(color: UIColor.clear, size: inputImage.size,  scale: inputImage.scale), let cgBgImage = bgImage.cgImage  else { return }
+        guard let bgImage = UIImage.imageFromColor(color: .black, size: inputImage.size,  scale: inputImage.scale), let cgBgImage = bgImage.cgImage  else { return }
 
         
         let input = CIImage(cgImage: image)
@@ -68,9 +74,127 @@ class ImageSegmentation: ObservableObject {
         let blendedImage = blendFilter.outputImage
         
         if let finalImage = blendedImage, let cgOutputImage = contex.createCGImage(finalImage, from: input.extent) {
-            outputImage = UIImage(cgImage: cgOutputImage)
+    
+            self.secondPartInputImage = UIImage(cgImage: cgOutputImage)
+            
+            faceContour()
         }
     }
     
+    private func faceContour() {
+
+        guard let image = secondPartInputImage.cgImage else { return }
+        
+        // Cria um request da biblioteca Vision
+        let request = VNDetectFaceLandmarksRequest(completionHandler: self.handleFaceContour)
+        
+        // Cria o request handler que é responsável por executar o request.
+        let requestHandler = VNImageRequestHandler(cgImage: image, orientation: .up, options: [:])
+        
+        // Executa de forma assíncrona.
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try requestHandler.perform([request])
+            } catch let error as NSError {
+                print(error.localizedDescription)
+                return
+            }
+        }
+    }
+    
+    private func handleFaceContour(request: VNRequest?, error: Error?) {
+        if let requestError = error as NSError? {
+            print(requestError.localizedDescription)
+            return
+        }
+        
+        
+        guard let observations = request?.results as? [VNFaceObservation] else { return }
+        guard let observation = observations.first else { return }
+        guard let face = observation.landmarks?.faceContour else { return }
+        
+        
+        
+        let faceContourPointsInImageCoordinate = face.pointsInImage(imageSize: inputImage.size)
+        
+        createPathFromImageCoordinates(points: faceContourPointsInImageCoordinate)
+    }
+    
+    
+    private func createPathFromImageCoordinates(points: [CGPoint]) {
+        
+        var normalizedPoints = points
+        
+        for i in 0..<normalizedPoints.count {
+            normalizedPoints[i].y = ((inputImage.size.height - normalizedPoints[i].y))
+        }
+        
+        var facePath = Path()
+        
+        facePath.addLines(normalizedPoints)
+        facePath.move(to: normalizedPoints.last ?? CGPoint.zero)
+        facePath.addLine(to: CGPoint(x: 0, y: normalizedPoints.last?.y ?? 0))
+        facePath.addLine(to: CGPoint(x: 0, y: 0))
+        facePath.addLine(to: CGPoint(x: inputImage.size.width, y: 0))
+        facePath.addLine(to: CGPoint(x: inputImage.size.width, y: normalizedPoints.first?.y ?? 0))
+        facePath.addLine(to: normalizedPoints.first ?? CGPoint.zero)
+        facePath.closeSubpath()
+        
+        faceContourPath = facePath
+        
+        DispatchQueue.main.async {
+            self.renderImage()
+        }
+    }
+    
+    
+    @MainActor private func renderImage() {
+        guard let facePath = faceContourPath else { return }
+        
+        let renderer = ImageRenderer(content: Image(uiImage: secondPartInputImage)
+            .frame(width: inputImage.size.width, height: inputImage.size.height)
+            .mask(alignment: .center) {
+                FaceContourShape(facePath: facePath)
+            })
+        outputImage = renderer.uiImage
+    }
+    
+    
+    func faceQuality() {
+        guard let image = inputImage.cgImage else { return }
+        
+        let faceDetectionRequest = VNDetectFaceCaptureQualityRequest(completionHandler: self.handleFaceQuality)
+        let requestHandler = VNImageRequestHandler(cgImage: image, orientation: .up, options: [:])
+        
+        
+        do {
+            try requestHandler.perform([faceDetectionRequest])
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    private func handleFaceQuality(request: VNRequest?, error: Error?) {
+        if let error = error as NSError? {
+            print(error.localizedDescription)
+            return
+        }
+        
+        if let results = request?.results as? [VNFaceObservation] {
+            quality = results.first?.faceCaptureQuality
+            print(results.first?.faceCaptureQuality ?? 0)
+        }
+    }
+}
+
+
+struct FaceContourShape: Shape {
+    
+    var facePath: Path
+    
+    func path(in rect: CGRect) -> Path {
+        return self.facePath
+    }
     
 }
--- Milho/View/ContentView.swift
+++ Milho/View/ContentView.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let image = UIImage(named: "teste")!
+    @ObservedObject var segmentation = ImageSegmentation()
+    @ObservedObject var imageQuality = FaceImageQuality()
+    @State var processedImage: UIImage?
+    @State var quality: Float?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear {
+            imageQuality.inputImage = image
+            imageQuality.faceQuality()
+            quality = imageQuality.quality
+        }
+        
+        if let imageQuality = self.quality {
+            Text("\(imageQuality)")
+        }
+        
+        if let processedImage {
+            Image(uiImage: processedImage)
+                .resizable()
+                .scaledToFit()
+        } else {
+            Button("Process Image") {
+                segmentation.inputImage = image
+                segmentation.segmentImage()
+                
+                if let outputImage = segmentation.outputImage {
+                    processedImage = outputImage
+                    imageQuality.inputImage = outputImage
+                    imageQuality.faceQuality()
+                    quality = imageQuality.quality
+                }
+            }
         }
     }
 }
