bd3de5fbe7d528adf746125793f2a509759e7fab
Ana Elisa <elisaslimao@hotmail.com>
2023-11-13 09:06:31 -0300

    developing palettes for computational vision

---
+++ Milho/PaletteScreen/Model/PaletteData.swift
@@ -0,0 +1,18 @@
+//
+//  Palette.swift
+//  Milho
+//
+//  Created by Ana Elisa Lima on 10/11/23.
+//
+
+import SwiftUI
+
+struct Palette: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Palette()
+}
